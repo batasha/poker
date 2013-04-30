af3eed21c5f6ad116f3de763562cef40132044cf
Connor Savage and Natasha Badillo <connor+natasha@appacademy.io>
2013-04-30 11:36:23 -0400

    Added draw_hand, init, size to hand.rb

--- lib/hand.rb
+++ lib/hand.rb
@@ -3,6 +3,18 @@ class Hand
   HAND_RANKINGS = [ :high_card, :pair, :three, :straight, :flush,
                     :str_flush ]
 
+  def self.draw_hand(deck)
+    Hand.new(deck)
+  end
 
+  attr_reader :cards
+
+  def initialize(deck)
+    @cards = deck.take(5)
+  end
+
+  def size
+    @cards.size
+  end
 
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -4,13 +4,23 @@ require "hand"
 describe Hand do
 
   describe "::draw_hand(deck)" do
-    it "calls #take on deck"
-    it "creates a five-card hand"
+    deck = Deck.new
+
+    it "calls #take on deck" do
+      deck.should_receive(:take).with(5)
+      Hand.draw_hand(deck)
+    end
+
+    it "creates a five-card hand" do
+      Hand.draw_hand(deck).size.should == 5
+    end
+
   end
 
-  describe "#initialize" do
-    it "creates a five-card hand by default"
-
+  describe "#initialize(deck)" do
+    it "creates a five-card hand by default" do
+      Hand.new(Deck.new).size.should == 5
+    end
   end
 
   describe "#discard(cards, deck)" do
