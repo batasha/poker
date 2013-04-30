bc1ec9a676575378970f32d53175a51938649e67
Connor Savage and Natasha Badillo <connor+natasha@appacademy.io>
2013-04-30 10:42:48 -0400

    Finish deck_spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -16,23 +16,37 @@ describe Deck do
     end
   end
 
+
+
   subject(:deck) { Deck.new }
+  subject(:cards) { [Card.new(:hearts, :ace), Card.new(:spades, :three),
+                     Card.new(:clubs, :jack), Card.new(:diamonds, :deuce)] }
 
   describe "#initialize" do
-    it "initializes with a full deck by default"
-    it "can be initialized with an array of cards"
+
+    its(:size) {should == 52}
+
+    it "can be initialized with an array of cards" do
+      Deck.new(cards).cards.should == cards
+    end
   end
 
   describe "#shuffle!" do
-    it "shuffles the deck"
+    it "shuffles the deck" do
+      deck.cards.should_not == deck.dup.shuffle!.cards
+    end
   end
 
   describe "#take(n)" do
-    it "removes n cards from the top of the deck"
+    it "removes n cards from the top of the deck" do
+      Deck.new(cards).take(3).should == cards[1..3]
+    end
   end
 
   describe "#return_cards(cards)" do
-    it "returns cards to the bottom of the deck"
+    it "returns cards to the bottom of the deck" do
+      Deck.new(cards).return(cards).cards.should == cards + cards
+    end
   end
 
 end
